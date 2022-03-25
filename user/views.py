b0d99f603429bbfadd98e9bf16886c0ddbebce80
sameer061 <70535612+sameer061@users.noreply.github.com>
2022-03-25 21:14:57 +0530

    first commit

---
+++ user/views.py
@@ -0,0 +1,55 @@
+from xml.sax.handler import property_interning_dict
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import Group
+from django.contrib.auth.decorators import login_required
+from .forms import CreateUserForm, UserUpdateForm, ProfileUpdateForm
+from django.core.exceptions import ObjectDoesNotExist
+
+# Create your views here.
+
+
+def register(request):
+    if request.method == 'POST':
+        form = CreateUserForm(request.POST)
+        if form.is_valid():
+            try:
+                user = form.save()
+                group = Group.objects.get(name='Customers')
+                user.groups.add(group)
+            except ObjectDoesNotExist:
+                print("created and  saved")
+                return redirect('user-login')
+    else:
+        form = CreateUserForm()
+    context = {
+        'form': form
+    }
+    return render(request, 'user/register.html', context)
+
+
+def profile(request):
+    context = {
+
+    }
+    return render(request, 'user/profile.html', context)
+
+
+def profile_update(request):
+    if request.method == 'POST':
+        u_form = UserUpdateForm(request.POST, instance=request.user)
+        p_form = ProfileUpdateForm(
+            request.POST, request.FILES, instance=request.user.profile)
+        if u_form.is_valid() and p_form.is_valid():
+            u_form.save()
+            p_form.save()
+            return redirect('user-profile')
+    else:
+        u_form = UserUpdateForm(instance=request.user)
+        p_form = ProfileUpdateForm(instance=request.user.profile)
+
+    context = {
+        'u_form': u_form,
+        'p_form': p_form,
+    }
+    return render(request, 'user/profile_update.html', context)
